22026df93f063cfabec8c2a2ec02ccb85943c212
Jenil Savla <jenilsavla20@gmail.com>
2022-03-19 06:01:07 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -58,6 +58,10 @@ class Dev(Configuration):
 
     AUTH_USER_MODEL = "blango_auth.User"
 
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+    ACCOUNT_ACTIVATION_DAYS = 7
+
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
 
